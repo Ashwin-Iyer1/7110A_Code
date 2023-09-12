fdbf456873772b3c762df1c1379047a9e3318338
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2023-09-12 09:24:18 -0500

    add some stuff for virtual skills

--- Virtual skills bot/main.py
+++ Virtual skills bot/main.py
@@ -17,19 +17,39 @@ if(pyautogui.locateOnScreen('0.png') is None):
     print("Cannot find score")
 else:
     Score = pyautogui.locateOnScreen('0.png')
+if(pyautogui.locateOnScreen('Reload.png') is None):
+    print("Reload button not found")
+else:
+    Reload = pyautogui.locateOnScreen('Reload.png')
+
+
 
 # %%
-found = False
-found2 = False
 def first_Balls():
+    pyautogui.click(Reload)
+    sleep(5)
     pyautogui.click(Start)
-    sleep(20)
+    sleep(1)
+    if(pyautogui.locateOnScreen('Stop.png') is None):
+        print("Stop button not found")
+        pyautogui.click(Start)
+    sleep(22)
     pyautogui.screenshot('screenshot.png', region=(Score))
     score = easyocr.Reader(['en']).readtext('screenshot.png', detail = 0)
     print(score[0])
-    if(score[0] < 30):
+    if(int(score[0]) < 29):
         pyautogui.click(Start)
         print("Points not high enough, running again")
+        sleep(.5)
+        if(pyautogui.locateOnScreen('Submit.png') is None):
+            print("Submit button not found")
+        else:
+            Submit = pyautogui.locateOnScreen('Submit.png')
+        if(pyautogui.locateOnScreen('Restart.png') is None):
+            print("Restart button not found")
+        else:
+            Restart = pyautogui.locateOnScreen('Restart.png')
+        pyautogui.click(Restart)
         sleep(2)
         first_Balls()
     else:
@@ -37,27 +57,23 @@ def first_Balls():
 
 
 def second_Balls():
-    for i in range(0, 40):
+    for i in range(0, 36):
         sleep(1)
         pyautogui.click(Load)
-    while(found != True and found2 != True):
-        if(pyautogui.locateOnScreen('Submit.png') is None):
-            print("Submit button not found")
-        else:
-            found = True
-            Submit = pyautogui.locateOnScreen('Submit.png')
-        if(pyautogui.locateOnScreen('Restart.png') is None):
-            print("Restart button not found")
-        else:
-            found2 = True
-            Restart = pyautogui.locateOnScreen('Restart.png')
+    if(pyautogui.locateOnScreen('Submit.png') is None):
+        print("Submit button not found")
+    else:
+        Submit = pyautogui.locateOnScreen('Submit.png')
+    if(pyautogui.locateOnScreen('Restart.png') is None):
+        print("Restart button not found")
+    else:
+        Restart = pyautogui.locateOnScreen('Restart.png')
     sleep(2)
     pyautogui.click(Submit)
     sleep(2)
     pyautogui.click(Restart)
     sleep(2)
     first_Balls()
-
 
 
 first_Balls()
